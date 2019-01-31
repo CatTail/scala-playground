a22589f731f37536e0ffa297c8b0daac0aa5abb6
Chiyu Zhong <zhongchiyu@gmail.com>
2019-01-31 20:06:08 +0800

    add MergeHub backpressure example

--- src/main/scala/com/example/playground/AlsoToApp.scala
+++ src/main/scala/com/example/playground/AlsoToApp.scala
@@ -2,17 +2,25 @@ package com.example.playground
 
 import akka.NotUsed
 import akka.stream.OverflowStrategy
-import akka.stream.scaladsl.{MergeHub, Sink, Source}
+import akka.stream.scaladsl.{Flow, MergeHub, Sink, Source}
 
-import scala.concurrent.Await
+import scala.concurrent.{Await, Future}
 import scala.concurrent.duration.Duration
+import scala.concurrent.ExecutionContext.Implicits.global
 
 object AlsoToContext extends CommonContext {
-  val sink: Sink[Int, NotUsed] = MergeHub
-    .source[Int]
+  val sink: Sink[Int, NotUsed] = MergeHub.source[Int]
     .mapAsync(1)(action)
     .log("MergeHub")
     .to(Sink.foreach(value => println(s"MergeHub: $value")))
+    .run()
+
+  val delayedSink: Sink[Int, NotUsed] = MergeHub.source[Int]
+    .mapAsync(1)(i => Future {
+      Thread.sleep(1000)
+      i
+    })
+    .to(Sink.foreach(value => println(s"delayedSink: $value")))
     .run()
 }
 
@@ -48,4 +56,29 @@ object AlsoToFailedMergeHubApp extends App with CommonContext {
   Await.result(queue.offer(1), Duration.Inf)
   Await.result(queue.offer(2), Duration.Inf)
   Await.result(queue.offer(3), Duration.Inf)
+}
+
+object AlsoToWithBackpressureApp extends App with CommonContext {
+  import AlsoToContext._
+
+  Source(1 to 64)
+    .alsoTo(delayedSink)
+    .to(Sink.foreach(println(_)))
+    .run()
+}
+
+object AlsoToMergeHubWithBackpressureApp extends App with CommonContext {
+  import AlsoToContext._
+
+  desc("multiple source write to single backpressure sink")
+
+  Source(1 to 32)
+    .alsoTo(delayedSink)
+    .to(Sink.foreach(value => println(s"first: $value")))
+    .run()
+
+  Source(1 to 32)
+    .alsoTo(delayedSink)
+    .to(Sink.foreach(value => println(s"second: $value")))
+    .run()
 }
