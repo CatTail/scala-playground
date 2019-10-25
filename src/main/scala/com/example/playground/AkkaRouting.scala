31f639bf1ca0caa83d46caefb3fd0bfa8ed27add
Chiyu Zhong <zhongchiyu@gmail.com>
2019-10-25 16:07:42 +0800

    add RoundRobinGroup example

--- src/main/scala/com/example/playground/AkkaRouting.scala
+++ src/main/scala/com/example/playground/AkkaRouting.scala
@@ -1,19 +1,35 @@
 package com.example.playground
 
-import akka.Done
 import akka.actor.{Actor, ActorRef, Props}
 import akka.event.LoggingReceive
-import akka.routing.RoundRobinPool
+import akka.routing.{Broadcast, RoundRobinGroup, RoundRobinPool}
 
 object AkkaRouting extends App with CommonContext {
-  val master = system.actorOf(Props[Master])
+  system.actorOf(Props[PoolMaster])
+  system.actorOf(Props[GroupMaster])
 }
 
-class Master extends Actor {
+class PoolMaster extends Actor {
   val router: ActorRef = context.actorOf(RoundRobinPool(5).props(Props[Worker]), "router")
 
   override def preStart(): Unit = {
     router ! "hello"
+    router ! Broadcast("broadcast hello")
+  }
+
+  override def receive: Receive = LoggingReceive(Actor.emptyBehavior)
+}
+
+class GroupMaster extends Actor {
+  val routees = List.fill(5) {
+    val r = context.actorOf(Props[Worker])
+    context.watch(r)
+  }
+  val router: ActorRef = context.actorOf(RoundRobinGroup(routees.map(_.path.toString)).props(), "router")
+
+  override def preStart(): Unit = {
+    router ! "hello"
+    router ! Broadcast("broadcast hello")
   }
 
   override def receive: Receive = LoggingReceive(Actor.emptyBehavior)
